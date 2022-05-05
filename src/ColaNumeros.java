80baf58574f77c3d5c7d9089f8245b08494f9e00
DanielCS17 <102037535+DanielCS17@users.noreply.github.com>
2022-05-05 11:43:31 +0100

    Feature: S: Lo siento Goyo pero no me sale :(

--- src/ColaNumeros.java
+++ src/ColaNumeros.java
@@ -1,2 +1,14 @@
 public class ColaNumeros {
+
+    private static final int capacidad_inicial = 1;
+    private Object []cola = null;
+    private int numObjects = 0;
+
+    public void enqueue(){
+
+    }
+
+    public void dequeue(){
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner sc = new Scanner(System.in);
+        boolean bucle = true;
+        int opcion;
+
+        while (bucle == true){
+            System.out.print("Introduce un número para añadirlo a la cola: ");
+            opcion = sc.nextInt();
+            sc.nextLine();
+            System.out.println("");
+        }
     }
 }
